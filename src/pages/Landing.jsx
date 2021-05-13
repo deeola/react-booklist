b0141974f7dfed13472c8b996c72a39f1d0cffd7
zorro1rr <zorro1rr@gmail.com>
2021-05-13 10:39:05 -0600

    changed landing div to section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,10 +3,10 @@ import Header from '../components/Header';
 
 export default function LandingPage() {
   return (
-    <div className="sm:grid grid-cols-layout">
+    <section className="sm:grid grid-cols-layout">
       <Header />
       <h1>Landing Page</h1>
       <h2>Welcome to the BookLists App</h2>
-    </div>
+    </section>
   );
 }
